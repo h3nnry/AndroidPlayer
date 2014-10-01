c78b5b0fce42f80d82fba14a13c86da9819b736e
h3nry <h3nnry84@gmail.com>
2014-10-02 00:08:38 +0300

    AndreidPlayer

---
+++ src/h3nry/andreidplayer/AndreidPlayerFragment.java
@@ -0,0 +1,47 @@
+package h3nry.andreidplayer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class AndreidPlayerFragment extends Fragment{
+	private AudioPlayer mPlayer = new AudioPlayer();
+	private Button mPlayButton;
+	private Button mStopButton;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_andreid_player, parent, false);
+		
+		mPlayButton = (Button)v.findViewById(R.id.andreidplayer_playButton);
+		mPlayButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mPlayer.play(getActivity());
+				
+			}
+		});
+		mStopButton = (Button)v.findViewById(R.id.andreidplayer_stopButton);
+		mStopButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mPlayer.stop();
+				
+			}
+		});
+		
+		return v;
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mPlayer.stop();
+	}
+}
+
